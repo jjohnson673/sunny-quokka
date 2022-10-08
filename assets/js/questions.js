0cb47917c6820ca577dc2c201b90beffddd73ff1
jjohnson673 <jkjohnson673@gmail.com>
2022-10-08 10:06:02 -0400

    added in questions

---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+var questions = [
+    {
+        title: 'When inserting Java Script into an HTML page, what is the proper tag used?',
+        choices: ['<js>', '<javascript>', '<script>', '<.js>'],
+        answer: '<script>',
+    },
+    {
+        title: 'How do you start a FOR loop?',
+        choices: ['for=(i=0); (i<=9)', 'for (i>=9;i++)', 'for (i=0;i<=9;i++)', 'for i=0, i<=9;i++'],
+        answer: 'for (i=0;i<=9;i++)',
+    },    {
+        title: 'Where is the correct place to insert Javascript?',
+        choices: ['Below </html>', 'Only the <body> section', 'Only the <head> section', 'Both <body> and <head> sections are acceptable' ],
+        answer: 'Both <body> and <head> sections are acceptable',
+    },    {
+        title: 'How do you create a function?',
+        choices: ['function myFunction()', 'function = myFunction()', 'function = my.Function', 'function=1.myFunction'],
+        answer: 'function myFunction()',
+    },    {
+        title: 'How do you write an array?',
+        choices: ['var=cars ["truck", "sedan", "convertible", "minivan"]', 'var cars ="truck", "sedan", "convertible", "minivan"', 'var cars = ["truck", "sedan", "convertible", "minivan"]', 'cars var = ["truck", "sedan", "convertible", "minivan"]'],
+        answer: 'var cars = ["truck", "sedan", "convertible", "minivan"]',
+    },
+]
